2ad670fca979dc36f6537078bda6061eba3de40d
miguel13 <migueltmz@outlook.es>
2024-02-25 10:15:25 +0100

    Este pequeño proyecto muestra cómo, al estar un miembro configurado con visibilidad de paquete, no puede ser accedido desde otro paquete. La Clase Casa, en el paquete ciudad, puede acceder al método restar de la Clase Calculadora, pues es público, pero no puede acceder al método sumar, pues tiene la visibilidad por defecto (de paquete)

---
+++ src/ciudad/Casa.java
@@ -0,0 +1,10 @@
+package ciudad;
+
+import herramientas.Calculadora;
+
+public class Casa {
+    public void contarElementos(){
+        Calculadora.restar(4,2);
+        Calculadora.sumar(4,2);
+    }
+}
---
+++ src/herramientas/Calculadora.java
@@ -0,0 +1,11 @@
+package herramientas;
+
+public class Calculadora {
+    static int sumar(int sumando1, int sumando2){
+        return sumando1 + sumando2;
+    }
+
+    public static int restar(int operando1, int operando2){
+        return operando1 - operando2;
+    }
+}
